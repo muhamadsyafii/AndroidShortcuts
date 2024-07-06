39726898912fb2cfffe62243ee7900091f561b08
Muhamad Syafii <muhamadsyafii4@gmail.com>
2024-07-06 13:33:40 +0700

    initial project

---
+++ app/src/main/java/id/syafii/androidshortcuts/presentation/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package id.syafii.androidshortcuts.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.syafii.androidshortcuts.R.id
+import id.syafii.androidshortcuts.R.layout
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContentView(layout.activity_main)
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
+      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+      insets
+    }
+  }
+}
